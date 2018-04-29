ac1324201728827ebe38938ca79032bde09f6cd1
Yann Druffin <yann.druffin@laposte.net>
2018-04-29 11:35:57 -0700

    Add command flags

--- cmd/deptree/main.go
+++ cmd/deptree/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+type nameFlag []string
+
+func (n *nameFlag) String() string {
+	return fmt.Sprintf("%v", *n)
+}
+
+func (n *nameFlag) Set(value string) error {
+	*n = append(*n, value)
+	return nil
+}
+
+func main() {
+	var (
+		path  string
+		names nameFlag
+	)
+
+	flag.StringVar(&path, "path", "./data", "The path to the CPAN folder.")
+	flag.Var(&names, "name", "Distribition names to resolve; you can define this flag multile time.")
+	flag.Parse()
+
+	fmt.Printf("%s \n", names)
 }
