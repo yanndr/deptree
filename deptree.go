6f683d314e301afe430148f9040616e85c04c586
Yann Druffin <yann.druffin@gmail.com>
2018-04-30 09:23:38 -0700

    remove useless json package

--- deptree.go
+++ deptree.go
@@ -1,10 +1,10 @@
 package deptree
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"sort"
-
-	"bitbucket.org/yanndr/deptree/json"
 )
 
 const (
@@ -15,19 +15,30 @@ const (
 
 //Resolver defined the methods of a dependency tree resolver.
 type Resolver interface {
-	Resolve(distributions ...string) ([]*Distribution, error)
+	Resolve(distributions ...string) (DependencyTree, error)
 }
 
-type Distribution struct {
+type DependencyTree []*distribution
+
+type distribution struct {
 	Name         string          `json:",omitempty"`
-	Dependencies []*Distribution `json:",omitempty"`
+	Dependencies []*distribution `json:",omitempty"`
+}
+
+func (d distribution) contains(dist *distribution) bool {
+	for _, dep := range d.Dependencies {
+		if dep.Name == dist.Name {
+			return true
+		}
+	}
+	return false
 }
 
 type perlDepTreeResolver struct {
 	path            string
 	distributionMap map[string]string
 	coreModules     []string
-	cache           map[string]*Distribution
+	cache           map[string]*distribution
 }
 
 //New returns an instance of a perl dependency tree resolver.
@@ -36,25 +47,25 @@ func New(path string) (Resolver, error) {
 		path: path,
 	}
 	distroMapPath := fmt.Sprintf("%s/%s", path, distroMapFile)
-	err := json.DecodeFromFile(&r.distributionMap, distroMapPath)
+	err := decodeFromFile(&r.distributionMap, distroMapPath)
 	if err != nil {
 		return nil, fmt.Errorf("error decoding the json file %s, %s", distroMapPath, err)
 	}
 
 	coreModulesPath := fmt.Sprintf("%s/%s", path, coreModulesFile)
-	err = json.DecodeFromFile(&r.coreModules, coreModulesPath)
+	err = decodeFromFile(&r.coreModules, coreModulesPath)
 	if err != nil {
 		return nil, fmt.Errorf("error decoding the json file %s, %s", coreModulesPath, err)
 	}
-	r.cache = make(map[string]*Distribution)
+	r.cache = make(map[string]*distribution)
 	return r, nil
 }
 
-func (r *perlDepTreeResolver) Resolve(distributions ...string) ([]*Distribution, error) {
-	var result []*Distribution
+func (r *perlDepTreeResolver) Resolve(distributions ...string) (DependencyTree, error) {
+	var result []*distribution
 	for _, d := range distributions {
 
-		distro := &Distribution{Name: d}
+		distro := &distribution{Name: d}
 
 		if deps, ok := r.cache[d]; ok {
 			distro = deps
@@ -71,7 +82,7 @@ func (r *perlDepTreeResolver) Resolve(distributions ...string) ([]*Distribution,
 		for _, dep := range dependencies {
 			deps, err := r.Resolve(dep)
 			for _, dep := range deps {
-				if !contains(distro.Dependencies, dep) {
+				if !distro.contains(dep) {
 					distro.Dependencies = append(distro.Dependencies, dep)
 				}
 			}
@@ -84,15 +95,6 @@ func (r *perlDepTreeResolver) Resolve(distributions ...string) ([]*Distribution,
 	}
 
 	return result, nil
-}
-
-func contains(ds []*Distribution, d *Distribution) bool {
-	for _, v := range ds {
-		if v.Name == d.Name {
-			return true
-		}
-	}
-	return false
 }
 
 func (r *perlDepTreeResolver) getDependencies(dist string) ([]string, error) {
@@ -123,10 +125,25 @@ func (dt *perlDepTreeResolver) getRequiresModules(dist string) (map[string]inter
 	}{}
 
 	path := fmt.Sprintf("%s/%s/%s", dt.path, dist, metaJSONFile)
-	err := json.DecodeFromFile(meta, path)
+	err := decodeFromFile(meta, path)
 	if err != nil {
 		return nil, err
 	}
 
 	return meta.Prereqs.Runtime.Requires, nil
+}
+
+func decodeFromFile(v interface{}, path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("error opening the file %s , %s", path, err)
+	}
+	decoder := json.NewDecoder(file)
+
+	err = decoder.Decode(v)
+	if err != nil {
+		return fmt.Errorf("error decoding the the file %s, %s", file.Name(), err)
+	}
+
+	return nil
 }
